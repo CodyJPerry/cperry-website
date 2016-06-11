08280605874cfdb4ea31fe337306a6d5db572d39
Cody Perry <codyperry65@yahoo.com>
2016-06-11 15:15:42 -0400

    Add Typejs

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Run function once document has been loaded
+  $(function(){
+      $("#typed").typed({
+        strings: ["^1000 Design and Development solutions."],
+        typeSpeed: 0
+      });
+  });
+
