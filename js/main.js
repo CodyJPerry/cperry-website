8757e0ee8500edee061b4c59e660597b4e04003a
Cody Perry <codyperry65@yahoo.com>
2016-06-11 22:56:06 -0400

    Build out new contact page

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 // Run function once document has been loaded
   $(function(){
+  		//home page
       $("#typed").typed({
         strings: ["^1000 Design and Development solutions."],
         typeSpeed: 0
       });
-      console.log("ok");
+
+      //Contact page
+      $("#contactText").typed({
+      	strings: ["^1000 Lets work together.", "Contact Us today to get started!"],
+      	typeSpeed: 0
+      });
 
   });
 
